9f45cb4d3bb6500715431ce63ee367916f1185f6
babafeng <babafeng2025@gmail.com>
2026-04-24 10:57:13 +0800

    update

--- internal/netmark/netmark.go
+++ internal/netmark/netmark.go
@@ -1,11 +1,24 @@
 package netmark
 
-import "net"
+import (
+	"net"
+	"sync/atomic"
+)
 
 const SelfBypassMark = 0x80
 
+var selfBypassEnabled atomic.Bool
+
+func EnableSelfBypassMark() {
+	selfBypassEnabled.Store(true)
+}
+
+func DisableSelfBypassMark() {
+	selfBypassEnabled.Store(false)
+}
+
 func ConfigureDialer(d *net.Dialer) {
-	if d == nil {
+	if d == nil || !selfBypassEnabled.Load() {
 		return
 	}
 	configureDialer(d)
